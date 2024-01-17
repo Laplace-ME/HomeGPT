97bd8bb581394de32a0079f4bfed48ed45f33c6f
Master_Laplace <guillaume.papineau@epitech.eu>
2024-01-17 22:00:00 +0100

    feat(root): Add installation and usage instructions, update prompts and commands

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # HomeGPT
 A voice assistante
+
+## Installation
+```bash
+git clone git clone git@github.com:Laplace-ME/HomeGPT.git
+cd HomeGPT
+pip install -r requirements.txt
+```
+
+## Usage
+```bash
+python main.py
+```
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## License
+[Apache License 2.0](https://choosealicense.com/licenses/apache-2.0/)
+
+## Authors
+- [Laplace-ME](https://github.com/Laplace-ME)
+
+## Acknowledgments
+- [HuggingFace](https://huggingface.co/)
+
+## TODO
+- [ ] Add a GUI (Graphical User Interface)
+- [ ] Add a TTS (Text To Speech)
+- [ ] Add a STT (Speech To Text)
+- [ ] Add a NLU (Natural Language Understanding)
+- [ ] Add a NLP (Natural Language Processing)
+- [ ] Add a NER (Named Entity Recognition)
+- [ ] Add a NED (Named Entity Disambiguation)
+
+## References
+- [HuggingFace](https://huggingface.co/)
+- [HuggingFace Transformers](https://huggingface.co/transformers/)
+- [HuggingFace Datasets](https://huggingface.co/datasets/)
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import openai
 import pyttsx3
 import sys
 import os
+import time
 
 load_dotenv()
 
@@ -46,6 +47,47 @@ Instructions :
 Voilà, une omelette aux tomates et aux œufs délicieuse et rapide à préparer ! J'espère que vous apprécierez ce plat. Si vous avez d'autres préférences ou des ingrédients spécifiques en tête, n'hésitez pas à me le faire savoir !
 """
 
+HISTORIQUE = []
+
+# "cuisine": "https://www.marmiton.org/recettes/",
+# "bricolage": "https://www.leroymerlin.fr/v3/p/idees-bricolage/",
+# "jardinage": "https://www.jardiland.com/idees-conseils/idees-jardin/",
+# "sport": "https://www.decathlon.fr/C-10851-idees-sport",
+# "informatique": "https://www.cours-gratuit.com/informatique",
+# "mécanique": "https://www.youtube.com/watch?v=dQw4w9WgXcQ", // it's a rickroll
+
+EXPERTS = {
+    "cuisine": os.getenv("CUISINE_PROMPT"),
+    "bricolage": os.getenv("BRICOLAGE_PROMPT"),
+    "jardinage": os.getenv("JARDINAGE_PROMPT"),
+    "sport": os.getenv("SPORT_PROMPT"),
+    "informatique": os.getenv("INFORMATIQUE_PROMPT"),
+    "mécanique": os.getenv("MECANIQUE_PROMPT"),
+}
+
+CURRENT_PROMPT = PROMPT
+
+COMMANDS = {
+    "bonjour": "Bonjour, comment allez-vous ?",
+    "ça va": "Je vais bien, merci !",
+    "merci": "Je vous en prie !",
+    "au revoir": "Au revoir, à bientôt !",
+    "quelle heure est-il": "Il est " + time.strftime("%H:%M"),
+    "quelle est la date": "Nous sommes le " + time.strftime("%d/%m/%Y"),
+    "quelle est la météo": "Il fait beau et chaud",
+    "quelle est la température": "Il fait 25 degrés",
+    "dis-moi une blague": "Qu'est-ce qui est jaune et qui attend ? Jonathan !",
+    "discute avec moi": "Je ne sais pas quoi dire",
+    "raconte-moi une histoire": "Il était une fois...",
+    "fais-moi une recette de cuisine": REPONSE,
+    "help": "Je peux vous donner l'heure, la date, la météo, la température, vous raconter une blague ou une histoire, ou vous faire une recette de cuisine",
+    "donne moi mon historique": HISTORIQUE,
+    "close": "Au revoir, à bientôt !",
+    "j'aimerais parler à un expert en": "Je ne suis pas un expert en {0}, mais je peux vous aider à trouver un expert en {0}.",
+    "redevient mon assistant": "Je suis de nouveau votre assistant !",
+    "redevient normal": "Je suis de nouveau normal !",
+}
+
 def get_openai_response(prompt: str, text: str) -> str:
     """_summary_
 
@@ -75,9 +117,6 @@ def text_to_speech(text: str, lang: str ='fr'):
     # Initialize the text-to-speech engine
     engine = pyttsx3.init()
 
-    # Get the system voices
-    voices = engine.getProperty('voices')
-
     # Use the system voice (you can change the index to select a different voice)
     engine.setProperty('voice', lang)
     # Set the rate of speech (optional)
@@ -97,26 +136,50 @@ def translate_voice_into_text(langue: str ='fr-FR'):
     Args:
         langue (str, optional): _description_. Defaults to 'fr-FR'.
     """
-    # recognizer = sr.Recognizer()
+    recognizer = sr.Recognizer()
 
-    # with sr.Microphone() as source:
-    while True:
-        print("Parlez maintenant...")
-        # recognizer.adjust_for_ambient_noise(source, duration=0.5)
-        # audio = recognizer.listen(source)
+    with sr.Microphone() as source:
+        while True:
+            print("Parlez maintenant...")
+            recognizer.adjust_for_ambient_noise(source, duration=0.5)
+            audio = recognizer.listen(source)
 
-        try:
-            # transcription = recognizer.recognize_google(audio, language=langue)
-            transcription = TRANSCRIPTION
-            print("Transcription : " + transcription)
-            # response = get_openai_response(PROMPT, transcription)
-            response = REPONSE
-            print("Réponse : " + response)
-            text_to_speech(response, langue.split("-")[0])
-        except sr.UnknownValueError:
-            print("Impossible de comprendre l'audio")
-        except sr.RequestError as e:
-            print(f"Erreur lors de la requête à l'API Google Speech Recognition : {e}")
+            try:
+                transcription = recognizer.recognize_google(audio, language=langue).lower()
+                if transcription.startswith("j'aimerais parler à un expert en"):
+                    if transcription.split("en ")[1] not in EXPERTS.keys():
+                        print("Transcription : " + transcription)
+                        text_to_speech("Je ne suis pas un expert en " + transcription.split("en ")[1] + ", mais je peux vous aider à trouver un expert en " + transcription.split("en ")[1] + ".")
+                        continue
+                    CURRENT_PROMPT = EXPERTS.get(transcription.split("en ")[1])
+                    print("Nouveau prompt : " + CURRENT_PROMPT)
+                    text_to_speech("Je vous mets en relation avec un expert en " + transcription.split("en ")[1] + ".")
+                    continue
+                elif transcription.startswith("redevient mon assistant") or transcription.startswith("redevient normal"):
+                    CURRENT_PROMPT = PROMPT
+                    print("Nouveau prompt : " + CURRENT_PROMPT)
+                    text_to_speech("Je suis de nouveau votre assistant !")
+                    continue
+                if COMMANDS.get(transcription) is not None:
+                    if transcription == "discute avec moi":
+                        HISTORIQUE.append(transcription)
+                    elif transcription == "close":
+                        print("Commande : " + transcription)
+                        text_to_speech(COMMANDS.get(transcription))
+                        break
+                    print("Commande : " + transcription)
+                    text_to_speech(COMMANDS.get(transcription))
+                    continue
+
+                print("Transcription : " + transcription)
+                # response = get_openai_response(CURRENT_PROMPT, transcription)
+                response = "Je n'ai pas compris"
+                print("Réponse : " + response)
+                text_to_speech(response, langue.split("-")[0])
+            except sr.UnknownValueError:
+                print("Impossible de comprendre l'audio")
+            except sr.RequestError as e:
+                print(f"Erreur lors de la requête à l'API Google Speech Recognition : {e}")
 
 if __name__ == "__main__":
     try:
